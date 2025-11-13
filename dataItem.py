5786d32319721c06195bd598f3df36f8e33154ed
Ben Tanguay <benjamin.tanguay@maine.edu>
2025-11-13 11:27:38 -0500

    Create dataItem.py
    Created and outlined the file datItem.py

---
+++ dataItem.py
@@ -0,0 +1,12 @@
+class DataItem:
+  def __init__ (self, line):
+    # self.movie_name
+    # self.genre
+    # self.release_date
+    # self.director
+    # self.revenue
+    # self.rating
+    # self.min_duration
+    # self.production_company
+    # self.quote
+    pass 
